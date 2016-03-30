8b9600a38aac5939b173348828e402a3e70d3c9f
Mats Högberg <mats@hgbrg.se>
2016-03-30 02:06:50 +0200

    Posts are now saved in a dictionary before being saved to the output folder

--- parse_posts.py
+++ parse_posts.py
@@ -72,36 +72,58 @@ def parse_posts(posts_folder, output_folder):
     generated html to the output folder. The posts have to have the file ending
     .markdown or .md.
 
-    :param posts_folder: The folder to read posts from.
-    :param output_folder: The folder to output html to.
+    :param posts_folder: The folder to read posts from. If this folder doesn't
+                         exist the script will exit without doing anything.
+    :param output_folder: The folder to output html to. Will be created if it
+                          doesn't exist.
     """
     if not os.path.exists(posts_folder):
         print('Error: Folder {} does not exist'.format(posts_folder))
         sys.exit(2)
 
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    posts = read_posts(posts_folder, output_folder)
+
+    for post in posts:
+        render_post(post, output_folder)
+
+    render_index(posts, output_folder)
+
+
+def read_posts(posts_folder, output_folder):
+    """
+    Scans the posts folder. All non-markdown files and folders will be copied
+    to the output folder without modification. Markdown files directly in the
+    posts folder will be parsed.
+
+    :param posts_folder: The folder to look for files in.
+    :param output_folder: The folder to copy files to.
+    :returns: A list of all posts found in the posts folder.
+    """
     posts = []
     for dir_entry in os.scandir(posts_folder):
         if not dir_entry.name.startswith('.') and dir_entry.name != 'drafts': # Ignore hidden files and folders. Will not work on Windows.
             if dir_entry.is_file():
                 filename, extension = os.path.splitext(dir_entry.name)
                 if extension == '.markdown' or extension == '.md':
-                    parse_post(dir_entry, output_folder)
-                    posts.append(filename + '.html')
+                    post = parse_post(dir_entry)
+                    posts.append(post)
                 else:
                     shutil.copy(dir_entry.path, output_folder)
             elif dir_entry.is_dir():
                 shutil.copytree(dir_entry.path, os.path.join(output_folder, dir_entry.name))
 
-    html = render_index(posts)
-    write_file('index.html', html, output_folder)
+    return posts
 
 
-def parse_post(dir_entry, output_folder):
+def parse_post(dir_entry):
     """
-    Parses a post from a markdown file and renders the result to a html file.
+    Parses a post written in markdown with optional metadata from a file.
 
     :param dir_entry: A directory entry for the post file to parse.
-    :param output_folder: The folder to write the rendered post file to.
+    :returns: A dictionary representing a single post.
     """
     f = open(dir_entry.path)
 
@@ -109,13 +131,13 @@ def parse_post(dir_entry, output_folder):
     content = f.read()
     html_content = mistune.markdown(content)
 
-    title = metadata['title'] if 'title' in metadata else os.path.splitext(dir_entry.name)[0]
+    # If there is no title attribute in the metadata the filename (without suffix) will be used.
+    if 'title' not in metadata:
+        metadata['title'] = os.path.splitext(dir_entry.name)[0]
 
-    renderer = pystache.Renderer(search_dirs='templates')
-    body = renderer.render_name('blog_post', {'content': html_content, 'metadata': metadata})
-    html = renderer.render_name('main', {'body': body, 'title': title})
-
-    write_file(title + '.html', html, output_folder)
+    post = {'content': html_content}
+    post.update(metadata)
+    return post
 
 
 def read_metadata(f):
@@ -151,26 +173,39 @@ def read_metadata(f):
     return metadata
 
 
-def render_index(posts):
+def render_post(post, output_folder):
+    """
+    Renders a post.
+
+    :param post: The post to use when rendering the template.
+    :param output_folder: The folder to write the rendered file to.
+    """
+    render_template('blog_post', {'title': post['title'], 'post': post}, output_folder)
+
+
+def render_index(posts, output_folder):
+    """
+    Renders an index file.
+
+    :param posts: The posts to use when rendering the template.
+    :param output_folder: The folder to write the rendered file to.
+    """
+    render_template('index', {'title': 'Index', 'posts': posts}, output_folder)
+
+
+def render_template(template, data, output_folder):
+    """
+    Renders a template and writes it to a new html file.
+
+    :param template: The name of the template to use when rendering.
+    :param data: The data to use when rendering.
+    :param output_folder: The folder to write the new html file to.
+    """
     renderer = pystache.Renderer(search_dirs='templates')
-    body = renderer.render_name('index', {'posts': posts})
-    html = renderer.render_name('main', {'body': body, 'title': 'Index'})
-    return html
-
-
-def write_file(filename, content, folder):
-    """
-    Writes a file to a folder.
-
-    :param filename: The filename of the new file.
-    :param content: The content of the new file.
-    :param folder: The folder to write the new file to.
-    """
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-
-    path = os.path.join(folder, filename)
-    open(path, 'w').write(content)
+    body = renderer.render_name(template, data)
+    html = renderer.render_name('main', {'body': body, 'title': data['title']})
+    path = os.path.join(output_folder, data['title'] + '.html')
+    open(path, 'w').write(html)
 
 
 if __name__ == '__main__':
