8e01b3f781fa88fc0aa82b36533a41f8332262db
Mats Högberg <mats@hgbrg.se>
2016-06-15 21:15:01 +0200

    Git counter now counts commits from all branches

--- src/build_blog.py
+++ src/build_blog.py
@@ -240,27 +240,26 @@ def get_github_commit_streak(api_key, email, verbose=False):
 
     for repo in repos:
         if verbose:
-            print('Fetching commits for {0}...'.format(repo['name']))
+            print('Fetching branches for {0}...'.format(repo['name']))
 
-        commits_url = repo['commits_url'].replace('{/sha}', '') + '?author=' + email
-        commits = json_request(commits_url, auth_header)
+        branch_url = repo['branches_url'].replace('{/branch}', '')
+        branches = json_request(branch_url, auth_header)
 
-        for commit in commits:
-            try:
-                if len(commit['parents']) <= 1:
-                    date = commit['commit']['author']['date'][:10]
-                    dates_with_commits.add(date)
-            except TypeError as e:
-                # Something missing from commit, disregard it.
-                pass
+        for branch in branches:
+            if verbose:
+                print('Fetching commits for {0}...'.format(branch['name']))
 
-    count = 0
-    date = datetime.date.today()
-    while date.strftime('%Y-%m-%d') in dates_with_commits:
-        count = count + 1
-        date = date - datetime.timedelta(days=1)
+            commits_url = '{0}?sha={1}&author={2}'.format(repo['commits_url'].replace('{/sha}', ''), branch['name'], email)
+            commits = json_request(commits_url, auth_header)
 
-    return count
+            for commit in commits:
+                try:
+                    if len(commit['parents']) <= 1:
+                        date = commit['commit']['author']['date'][:10]
+                        dates_with_commits.add(date)
+                except TypeError as e:
+                    # Something missing from commit, disregard it.
+                    pass
 
 
 def get_last_song(api_key):
